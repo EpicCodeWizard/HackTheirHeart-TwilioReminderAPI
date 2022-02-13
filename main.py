32b603889df626e429a9441f4f12d25368f5f505
EpicCodeWizard <83260377+EpicCodeWizard@users.noreply.github.com>
2022-02-12 23:29:01 -0800

    init

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+from twilio.rest import Client
+from replit import db
+import datetime
+import os
+
+app = Flask(__name__, template_folder="", static_folder="")
+client = Client(os.environ["TWILIO_ACCOUNT_SID"], os.environ["TWILIO_AUTH_TOKEN"])
+
+@app.route("/date/create", methods=["POST"])
+def createDate():
+    data = request.json
+    client.messages.create(messaging_service_sid=os.environ["TWILIO_MESSAGING_SERVICE_SID"], body="Don't miss your date in 10 minutes!", send_at=data["date"]+":00Z", schedule_type="fixed", to="+"+str(data["phone1"]))
+    client.messages.create(messaging_service_sid=os.environ["TWILIO_MESSAGING_SERVICE_SID"], body="Don't miss your date in 10 minutes!", send_at=data["date"]+":00Z", schedule_type="fixed", to="+"+str(data["phone2"]))
+    db['dates'].append(data)
+    return ('', 204)
+
+@app.route("/date/info/<phone1>/<phone2>", methods=["GET"])
+def getDate(phone1, phone2):
+    for date in db['dates']:
+        tempdate = dict(date)
+        if tempdate['phone1'] == int(phone1) and tempdate['phone2'] == int(phone2):
+            return tempdate
+    return ('', 204)
+
+app.run(host="0.0.0.0")
